d8b39765768fb0c9052868739e1dfb9c98578019
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-06-05 18:36:29 +0000

    terraform automation script

---
+++ apply-terraform.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Set Terraform variables
+PROJECT_ID="commit-smart"
+REGION="us-east1"
+TF_DIR="Terraform/"
+
+# Navigate to the Terraform directory
+cd $TF_DIR
+
+# Initialize Terraform
+terraform init
+
+# Validate the Terraform configuration
+terraform validate
+
+# Plan the Terraform changes
+terraform plan -out=tfplan \
+  -var="project_id=$PROJECT_ID" \
+  -var="region=$REGION"
+
+# Apply the Terraform changes
+terraform apply tfplan
+
+# Clean up the Terraform plan file
+rm tfplan
